77bd5995a4c38db44c91e6594cf3607c0a273fc3
IsaacBenavides <isaacxzx@gmail.com>
2022-02-04 12:22:56 -0500

    fixeado problema de no re-renderizado del componente

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,32 @@
-import Tareas from "./clases/Tareas"
+import Tareas from "./clases/Tareas";
 import { useState } from "react";
 
 let tareas = new Tareas();
 
 function App() {
   const [allTask, setAllTask] = useState([]);
-  function setTasks(){
+
+  function setTasks() {
     let nombre = document.getElementById("nombre").value;
-        let descripcion = document.getElementById("descripcion").value;
-        let currentTask = {
-          nombre:nombre,
-          descripcion:descripcion
-        }
-        tareas.addTarea(currentTask);
-        setAllTask(tareas.tareas);
+    let descripcion = document.getElementById("descripcion").value;
+    let currentTask = {
+      nombre: nombre,
+      descripcion: descripcion,
+    };
+    tareas.addTarea(currentTask);
+    setAllTask((_) => [...tareas.tareas]);
   }
-  return(
+
+  return (
     <div>
-      {allTask.map((task, index) => <div key={index}>{task.nombre}</div>)}
+      {allTask.map((task, index) => (
+        <div key={index}>{task.nombre}</div>
+      ))}
       <input type="text" placeholder="nombre" id="nombre" />
-      <input type="text" placeholder="Descripcion" id="descripcion"/>
-      <button onClick={setTasks}>
-        Agregar Tarea
-      </button>
+      <input type="text" placeholder="Descripcion" id="descripcion" />
+      <button onClick={setTasks}>Agregar Tarea</button>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
