de1844f2f0694925764588e17baabcd77660c7df
IsaacBenavides <isaacxzx@gmail.com>
2022-02-04 08:24:50 -0500

    clase

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,29 @@
+import Tareas from "./clases/Tareas"
+import { useState } from "react";
+
+let tareas = new Tareas();
 
 function App() {
+  const [allTask, setAllTask] = useState([]);
+  function setTasks(){
+    let nombre = document.getElementById("nombre").value;
+        let descripcion = document.getElementById("descripcion").value;
+        let currentTask = {
+          nombre:nombre,
+          descripcion:descripcion
+        }
+        tareas.addTarea(currentTask);
+        setAllTask(tareas.tareas);
+  }
   return(
-    <h1>Hello World</h1>
+    <div>
+      {allTask.map((task, index) => <div key={index}>{task.nombre}</div>)}
+      <input type="text" placeholder="nombre" id="nombre" />
+      <input type="text" placeholder="Descripcion" id="descripcion"/>
+      <button onClick={setTasks}>
+        Agregar Tarea
+      </button>
+    </div>
   )
 }
 
---
+++ src/clases/Tareas.jsx
@@ -0,0 +1,27 @@
+export default class Tareas {
+    static idTarea;
+    constructor(){
+        this.idTarea = 1;
+        this.tareas = [];
+    }
+    
+    addTarea(tarea) {
+        let obj = {
+            id: this.idTarea,
+            ...tarea
+        }
+        this.tareas.push(obj);
+        this.idTarea++;
+    };
+
+    eliminarTarea = (id) => {
+        let index = this.tareas.findIndex((tarea)=>tarea.id==id)
+        this.tareas.splice(index,1)
+    }
+
+    modificarTarea = (id,status) => {
+        let index = this.tareas.findIndex((tarea)=>tarea.id==id)
+    }
+
+}
+
